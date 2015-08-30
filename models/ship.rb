527f2d9e7d496757669fb6d00ad8ba63155c3437
Anthony Castelli <anthonycastelli01@gmail.com>
2015-08-30 00:24:30 -0700

    Add ability for ship to keep track of coordinates

--- models/ship.rb
+++ models/ship.rb
@@ -3,5 +3,14 @@ class Ship
 
   def initialize(options = {})
     @type = options.fetch(:type, "")
+    @coordinates = []
+  end
+
+  def store_coordinates(coordinates)
+    @coordinates = coordinates
+  end
+
+  def at_location?(coordinate)
+    @coordinates.include?(coordinate)
   end
 end
