60e24fd187603b6b2c084c8a83bd0c5c38e7160a
Anthony Castelli <anthonycastelli01@gmail.com>
2015-08-29 23:49:03 -0700

    Add expectations for ship class

---
+++ spec/ship_spec.rb
@@ -0,0 +1,15 @@
+require_relative '../models/ship'
+
+describe 'SHIP CLASS' do
+  let(:ship) { Ship.new("Battleship") }
+
+  describe "#Type" do
+    it "should be stored as an instance variable" do
+      expect(shipt).to respond_to(:type)
+    end
+
+    it "stores a string" do
+      expect(ship.type).to eq("Battleship")
+    end
+  end
+end
