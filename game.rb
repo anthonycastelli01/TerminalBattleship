2e0f954a0854c2739b1c4aaecca214b4b8a3666b
Anthony Castelli <anthonycastelli01@gmail.com>
2015-09-04 16:38:42 -0700

    Reformat game display and implement success/fail display

--- game.rb
+++ game.rb
@@ -1,5 +1,6 @@
 require_relative 'models/board'
 require_relative 'views/board_view'
+require_relative 'modules/coordinates'
 
 class Game
   attr_reader :player_board, :computer_board
@@ -7,11 +8,40 @@ class Game
   def initialize
     @player_board = Board.new
     @computer_board = Board.new
+    @game_state = false
+    @exit_commands = ['exit', 'quit']
   end
 
   def run
-    welcome
     show_boards
+    welcome
+
+    i = 0
+    while @game_state == false
+      target = get_player_target
+
+      if @exit_commands.include?(target)
+        BoardView.thanks(@game_state)
+        break
+      elsif Coordinates.valid_string_coordinates?(target)
+        BoardView.reset_display
+        show_boards
+        puts "That's a valid coordinate!"
+        i += 1
+      else
+        BoardView.reset_display
+        show_boards
+        BoardView.reject_coordinates(target)
+      end
+
+      if i > 10
+        @game_state = true
+      elsif i < 0
+        i = 1
+      end
+    end
+
+    BoardView.thanks(@game_state)
   end
 
   private
@@ -22,6 +52,11 @@ class Game
   end
 
   def show_boards
+    BoardView.reset_display
     BoardView.display_boards(@player_board, @computer_board)
+  end
+
+  def get_player_target
+    return BoardView.get_target
   end
 end
